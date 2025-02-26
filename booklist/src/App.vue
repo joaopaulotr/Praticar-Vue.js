ac2e050368005d8e5186d12641df8c0367f6e884
João Paulo <joaopaulotraguetta14@gmail.com>
2025-02-25 21:29:09 -0300

    cursofinalizado

---
+++ booklist/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+  let count = 0;
+
+</script>
+
+<template>
+  <div>
+    
+    <h1>{{ count }}</h1>
+    <button @click="count++ ">Incrementar +</button>
+
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+
+</style>
